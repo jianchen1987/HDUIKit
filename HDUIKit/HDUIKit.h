cea093133b54500eed0e6250606095dce44cdf6d
VanJay <wangwanjie1993@gmail.com>
2020-03-12 11:39:07 +0800

    published HDUIKit 0.4.6

--- HDUIKit/HDUIKit.h
+++ HDUIKit/HDUIKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDUIKit_VERSION = @"0.4.5";
+static NSString *const HDUIKit_VERSION = @"0.4.6";
 
 #if __has_include("HDDispatchMainQueueSafe.h")
 #import "HDDispatchMainQueueSafe.h"
