2c9f6b3ccf9c292357aace16d4776953d7c1cbba
肖志勇 <xiaozy@kh-super.com>
2022-10-09 17:54:40 +0800

    published HDUIKit 1.7.17

--- HDUIKit/HDUIKit.h
+++ HDUIKit/HDUIKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDUIKit_VERSION = @"1.7.16";
+static NSString *const HDUIKit_VERSION = @"1.7.17";
 
 #if __has_include("HDKeyboardManager.h")
 #import "HDKeyboardManager.h"
