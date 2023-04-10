494982b3293b728af9060051beaf503f57f2a917
肖志勇 <xiaozy@kh-super.com>
2023-04-10 10:45:44 +0800

    published HDUIKit 1.9.3

--- HDUIKit/HDUIKit.h
+++ HDUIKit/HDUIKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDUIKit_VERSION = @"1.9.2";
+static NSString *const HDUIKit_VERSION = @"1.9.3";
 
 #if __has_include("HDKeyboardManager.h")
 #import "HDKeyboardManager.h"
